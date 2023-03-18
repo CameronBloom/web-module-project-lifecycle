3791039b686c50b11fd3abadbca6c0e79017a351
Cameron <cameron.morgan@skiff.com>
2023-03-17 21:38:33 -0500

    Completed the axios API call, which returns the initial todos list data.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
-
-// const URL = 'http://localhost:9000/api/todos'
+import axios from 'axios'
+const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    }
+    this.getTodos = this.getTodos.bind(this);
+  }
+  // runs after first render
+  componentDidMount() {
+    this.getTodos();
+
+  }
+
+  // helper functions
+  getTodos = () => {
+    axios.get(URL)
+      .then((res) => {
+        this.setState({ ...this.state, todos: res.data.data })
+        return res.data.data;
+      })
+      .then((res) => { console.log(res) })
+      .catch(err => console.error(err))
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <h1>Welcome</h1>
+      </div>
+
+    )
+
   }
 }
